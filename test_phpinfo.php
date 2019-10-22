ddc253349b627da620b6d0aeaff887b733480e70
6239010014 <51111582+6239010014@users.noreply.github.com>
2019-10-22 08:25:37 +0700

    Update test_phpinfo.php

--- test_phpinfo.php
+++ test_phpinfo.php
@@ -1,7 +1,3 @@
 <?
-echo "Hello World"();
-echo "Today is ".date(Y/m/d)."<br>";
-echo "Today is ".date(Y/m/d)."<br>";
-echo "Today is ".date(Y/m/d)."<br>";
-echo "Today is ".date("1");
+php phpinfo();
 ?>
